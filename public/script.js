268d267795e09e727ce006cc4bcb467ab33a3a83
Vedant-Pote <vedant00101@gmail.com>
2026-03-16 10:27:38 +0530

    Added specs and api backend

--- public/script.js
+++ public/script.js
@@ -12,10 +12,29 @@ const div = document.createElement("div");
 div.className = "product";
 
 div.innerHTML = `
+<a href="product.html?id=${product.id}">
 <img src="${product.image}">
 <h3>${product.name}</h3>
+</a>
+
 <p class="price">₹${product.price}</p>
-<button onclick='addToCart("${product.name}",${product.price})'>Add to Cart</button>
+<p class="desc">${product.description}</p>
+
+<ul class="specs">
+${product.specs?.processor ? `<li><b>Processor:</b> ${product.specs.processor}</li>` : ""}
+${product.specs?.ram ? `<li><b>RAM:</b> ${product.specs.ram}</li>` : ""}
+${product.specs?.storage ? `<li><b>Storage:</b> ${product.specs.storage}</li>` : ""}
+${product.specs?.connectivity ? `<li><b>Connectivity:</b> ${product.specs.connectivity}</li>` : ""}
+${product.specs?.battery ? `<li><b>Battery:</b> ${product.specs.battery}</li>` : ""}
+${product.specs?.dpi ? `<li><b>DPI:</b> ${product.specs.dpi}</li>` : ""}
+${product.specs?.switches ? `<li><b>Switches:</b> ${product.specs.switches}</li>` : ""}
+${product.specs?.speed ? `<li><b>speed:</b> ${product.specs.speed}</li>` : ""}
+${product.specs?.sensors ? `<li><b>Sensors:</b> ${product.specs.sensors}</li>` : ""}
+</ul>
+
+<button onclick='addToCart("${product.name}",${product.price})'>
+Add to Cart
+</button>
 `;
 
 container.appendChild(div);
--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-// Fake database (JSON)
+// Data Storage
+let users = [];
+let orders = [];
+
+
 let products = [
 
 {
@@ -44,7 +48,7 @@ battery:"5000mAh"
 id:3,
 name:"Headphones",
 price:3000,
-image:"https://images.unsplash.com/photo-1518443895914-6bce8bfc9c2f",
+image:"https://hammeronline.in/cdn/shop/files/Bash_2.0_Bluetooth_Headphones.webp?v=1726899059",
 description:"Wireless headphones with noise cancellation and deep bass.",
 specs:{
 type:"Over-Ear",
@@ -58,7 +62,7 @@ weight:"250g"
 id:4,
 name:"Smart Watch",
 price:7000,
-image:"https://images.unsplash.com/photo-1516574187841-cb9cc2ca948b",
+image:"https://www.leafstudios.in/cdn/shop/files/1_1099cd20-7237-4bdf-a180-b7126de5ef3d_1000x.png?v=1722230645",
 description:"Fitness smartwatch with heart rate monitoring and GPS.",
 specs:{
 display:"1.8 inch AMOLED",
@@ -72,7 +76,7 @@ sensors:"Heart Rate, SpO2"
 id:5,
 name:"Gaming Mouse",
 price:1500,
-image:"https://images.unsplash.com/photo-1587202372775-e229f172b9d7",
+image:"https://m.media-amazon.com/images/I/61AcT0ZuO3L.jpg",
 description:"Ergonomic gaming mouse designed for high precision and fast response.",
 specs:{
 dpi:"16000 DPI",
@@ -86,16 +90,119 @@ connectivity:"USB Wired"
 id:6,
 name:"Mechanical Keyboard",
 price:4500,
-image:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97",
+image:"https://m.media-amazon.com/images/I/61P7MvyRbUL.jpg",
 description:"Mechanical keyboard with RGB lighting and tactile switches.",
 specs:{
 switches:"Blue Mechanical Switches",
 lighting:"RGB Backlit",
 keys:"104 Keys",
 connection:"USB"
-}
-}
+},
+},
 
+{
+id:7,
+name:"Wireless Earbuds",
+price:4500,
+image:"https://images.unsplash.com/photo-1590658268037-6bf12165a8df",
+description:"Compact wireless earbuds with deep bass and active noise cancellation.",
+specs:{
+connectivity:"Bluetooth 5.3",
+battery:"24 hours with charging case"
+}
+},
+
+{
+id:8,
+name:"4K Monitor",
+price:22000,
+image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG0tlspcd1sq4N_7BeNWKek9jacpYr72PeaQ&s",
+description:"Ultra HD 4K monitor ideal for gaming, programming and graphic design.",
+specs:{
+display:"27 inch 4K UHD",
+refreshRate:"144Hz",
+connectivity:"HDMI / DisplayPort"
+}
+},
+
+{
+id:9,
+name:"Portable SSD",
+price:8500,
+image:"https://i.pcmag.com/imagery/reviews/00jR5OEn9d3h9M6qWvqGNd9-1..v1569469917.jpg",
+description:"High speed portable SSD for storing large files and fast data transfer.",
+specs:{
+storage:"1TB",
+connectivity:"USB-C",
+speed:"1050MB/s"
+}
+},
+
+{
+id:10,
+name:"Gaming Headset",
+price:3500,
+image:"https://images.unsplash.com/photo-1599669454699-248893623440",
+description:"Comfortable gaming headset with surround sound and noise-canceling microphone for immersive gaming sessions.",
+specs:{
+connectivity:"3.5mm Jack / USB",
+audio:"7.1 Surround Sound",
+microphone:"Noise Cancelling"
+}
+},
+
+{
+id:11,
+name:"Wireless Charger",
+price:1200,
+image:"https://ambraneindia.com/cdn/shop/files/wireless-phone-charger.webp?v=1763451235&width=700",
+description:"Fast wireless charging pad compatible with smartphones and wireless charging enabled devices.",
+specs:{
+power:"15W Fast Charging",
+compatibility:"Qi Enabled Devices",
+connectivity:"USB-C"
+}
+},
+
+{
+id:12,
+name:"USB-C Hub",
+price:2200,
+image:"https://mm.digikey.com/Volume0/opasdata/d220001/medias/images/5881/MFG_5G3AGBB-USB-C-HUB.jpg",
+description:"Multi-port USB-C hub that expands your laptop connectivity with HDMI, USB ports and SD card reader.",
+specs:{
+ports:"HDMI, 3x USB, SD Card",
+compatibility:"USB-C Laptops",
+material:"Aluminium"
+}
+},
+
+{
+id:13,
+name:"Ipad",
+price:30000,
+image:"https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/ipad-air-storage-select-202405-13inch-starlight-wifi_FMT_WHH?wid=1280&hei=720&fmt=p-jpg&qlt=80&.v=TENLTVRoeFdHUUI5ZE1ZZmxpQUlNMm5pQUoxb0NIVEJFSjRVRzZ4dzV5VG9xblRoSytlREYxb2tmSnZTRlIvWmxzVWxuaTA3UGxIdzhKNUtuSEF5VlVtYW1kMXNQLzdDd2NuUVFzTDNlZmEyTE1mSHgwMHh1SlVoUDJNTksyTUZnWG04VnYwV0NCYVhKcVlFN0RlblJR&traceId=1",
+description:"Apple iPad Air 13″ (M2): Liquid Retina Display, 512GB, Landscape 12MP Front Camera / 12MP Back Camera, Wi-Fi 6E, Touch ID, All-Day Battery Life — Space Grey ",
+specs:{
+ram: "12 GB",
+storage: "512 Gb",
+connectivity: "Wifi 6e"
+
+}
+},
+
+{
+id:14,
+name:"Router",
+price:4800,
+image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZld5_-WJx8gZ1SNjZw8pNjZUzN6FZrBUETA&s",
+description:"High speed dual band WiFi router suitable for gaming, streaming and smart home devices.",
+specs:{
+speed:"3000 Mbps",
+bands:"Dual Band 2.4GHz / 5GHz",
+ports:"4 Gigabit LAN Ports"
+}
+}
 ];
 
 // GET all products
@@ -103,13 +210,86 @@ app.get("/products", (req, res) => {
   res.json(products);
 });
 
-// Add new product
-app.post("/products", (req, res) => {
-  const product = req.body;
-  products.push(product);
-  res.json(product);
+/* -------------------- SIGNUP -------------------- */
+
+app.post("/signup",(req,res)=>{
+
+const {name,email,password} = req.body;
+
+const existingUser = users.find(u => u.email === email);
+
+if(existingUser){
+return res.status(400).json({message:"User already exists"});
+}
+
+users.push({name,email,password});
+
+res.json({message:"Signup successful"});
+
+})
+
+/* -------------------- SIGNUP -------------------- */
+
+app.post("/signup",(req,res)=>{
+
+const {name,email,password} = req.body;
+
+const existingUser = users.find(u => u.email === email);
+
+if(existingUser){
+return res.status(400).json({message:"User already exists"});
+}
+
+users.push({name,email,password});
+
+res.json({message:"Signup successful"});
+
+})
+
+/* -------------------- LOGIN -------------------- */
+
+app.post("/login",(req,res)=>{
+
+const {email,password} = req.body;
+
+const user = users.find(u => u.email === email && u.password === password);
+
+if(user){
+res.json({message:"Login successful"});
+}else{
+res.status(401).json({message:"Invalid email or password"});
+}
+
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+
+/* -------------------- ADMIN ADD PRODUCT -------------------- */
+
+app.post("/add-product",(req,res)=>{
+
+const product = req.body;
+
+products.push(product);
+
+res.json({
+message:"Product added successfully",
+product
+});
+
+});
+
+/* -------------------- GET ALL ORDERS -------------------- */
+
+app.get("/orders",(req,res)=>{
+res.json(orders);
+});
+
+app.post('/order', (req, res) => {
+  res.json(orders)
+});
+
+/* -------------------- START SERVER -------------------- */
+
+app.listen(3000,()=>{
+console.log("Server running on port 3000");
 });
